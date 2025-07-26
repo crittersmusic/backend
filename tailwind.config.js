2469612699f60d0e25f69c7d58b1ee7eae561a67
crittersmusic <thecrittersmusic@gmail.com>
2025-07-26 12:50:40 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,17 @@
-
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        accent: '#90cdf4', // soft light blue
+        background: '#1a202c' // dark mode base
+      },
+      borderRadius: {
+        xl: '1rem',
+        '2xl': '1.5rem'
+      }
+    }
+  },
+  plugins: []
+}
